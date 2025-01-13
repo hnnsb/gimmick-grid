a98d147940dbdca102511a0c74da867caf8f2938
hnnsb <hannes.b@wtnet.de>
2025-01-13 22:33:25 +0100

    delete angular app

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import {Routes} from '@angular/router';
-import {HomePageComponent} from "./pages/home-page/home-page.component";
-import {TeamGeneratorComponent} from "./pages/team-generator/team-generator.component";
-
-export const routes: Routes = [
-  {path: "team-generator", component: TeamGeneratorComponent},
-  {path: "**", component: HomePageComponent},
-];
